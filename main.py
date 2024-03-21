99ea268691b74e19ee680c84bcaa1efd70854ce5
OrsoF <orso.forghieri@gmail.com>
2024-03-21 11:30:10 +0100

    Update

---
+++ main.py
@@ -0,0 +1,6 @@
+# Run main.py to run an experiment.
+
+from utils.experience_class import Experience
+
+exp = Experience()
+exp.run()
---
+++ reset.py
@@ -0,0 +1,5 @@
+# Reset the experience environment.
+
+from utils.data_management import reset
+
+reset()
